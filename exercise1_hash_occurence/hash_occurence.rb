74ff5539c09eddd3e7cb9e1dddb732ac09a6c1d9
ekansh <ekansin05@gmail.com>
2022-06-18 18:23:03 +0530

    exercise2

--- exercise1_hash_occurence/hash_occurence.rb
+++ exercise1_hash_occurence/hash_occurence.rb
@@ -1,19 +1,23 @@
+# Using Ruby version: ruby 2.7.1p83 (2020-03-31 revision a0c7c23c9c) [x86_64-linux]
 class String
-  def hash_occurences
-    # defining an empty hash with default as 0
-    frequency_table = Hash.new(0)
-  
-    # check if character is alphabet and increase count
-    self.each_char do |character|
-      if character =~ /[[:alpha:]]/
-        frequency_table[character] += 1
+    def hash_occurences
+      # defining an empty hash with default as 0
+      frequency_table = Hash.new(0)
+    
+      # check if character is alphabet and increase count
+      self.each_char do |character|
+        if character =~ /[[:alpha:]]/
+          frequency_table[character] += 1
+        end
       end
+    frequency_table
     end
-    print frequency_table
-  end
 end
-  
-puts 'Please provide an input'
+    
 input = gets.chomp
-print input.hash_occurences
-  
+if input.length == 0 
+  puts 'Please provide an input'
+else
+  print input.hash_occurences
+end  
+    
