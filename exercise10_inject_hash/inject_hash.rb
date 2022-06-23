d7aa0ff635f25379b1302d579b1e650d90a844e8
ekansh <ekansin05@gmail.com>
2022-06-23 11:18:00 +0530

    bugs fixed

--- exercise10_inject_hash/inject_hash.rb
+++ exercise10_inject_hash/inject_hash.rb
@@ -1,16 +1,10 @@
 class Array
-  def hash_by_len
-    count_by_length = Hash.new { |hash, key| hash[key] = [] }
-    each { |item| count_by_length[item.length].push(item) }
-    return count_by_length
-  end
-
   def hash_odd_even
-    frequency_hash = self.hash_by_len
-    return frequency_hash.inject(Hash.new {|h,k| h[k] = [] }) do |odd_even, item|
-      odd_even[item[0].odd? ? "odd" : "even"] << item[1]
+    self.group_by(&:length)
+    odd_even_hash = inject(Hash.new {|h,k| h[k] = [] }) do |odd_even, item|
+      odd_even[item[0].to_i.odd? ? "odd" : "even"] << item[1]
       odd_even
-    end
+    end 
   end
 end
 
