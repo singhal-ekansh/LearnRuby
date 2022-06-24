9d2c3fd2926d9d196fcb8a58b6366cc48d2eabf8
ekansh <ekansin05@gmail.com>
2022-06-24 15:56:46 +0530

    highlight words

--- exercise16_search_highlight/seach_highlight_regex.rb
+++ exercise16_search_highlight/seach_highlight_regex.rb
@@ -1,3 +1,15 @@
 class String
   def highlight_word(word)
-    gsub()
+    @count = 0
+    gsub!(Regexp.new(word,true)) do |match|
+      @count += 1
+      match = "(#{match})"
+    end   
+  end
+  def to_s
+    puts self
+    puts "Total occurrences found: #{@count}"
+  end
+end
+
+"Can can yoi can can canner".highlight_word('can').to_s
