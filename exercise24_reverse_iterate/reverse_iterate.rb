388bd45be1f903d3b46e0bfe19e7c6b0fa35908a
ekansh <ekansin05@gmail.com>
2022-07-05 11:56:48 +0530

    bugs

--- exercise24_reverse_iterate/reverse_iterate.rb
+++ exercise24_reverse_iterate/reverse_iterate.rb
@@ -1,11 +1,16 @@
+# Using Ruby version: ruby 2.7.1p83 (2020-03-31 revision a0c7c23c9c) [x86_64-linux]
+# Your Ruby code here
 class Array
   def reverse_iterate(&block)
     return if empty?
-
-    top = shift
+    
+    yield pop
     reverse_iterate(&block)
-    yield top
   end
 end
 
-[1,2,3,4,5,6].reverse_iterate {|i| print "#{i} "}
+if ARGV.length.zero?
+  puts 'Please provide an input'
+else
+  ARGV[0][1..-1].split(',').map {|ele| ele.strip.gsub(/'/,'')}.reverse_iterate {|i| print "#{i} "}
+end
--- exercise25_sales_tax/sales_tax.rb
+++ exercise25_sales_tax/sales_tax.rb
@@ -1,47 +1,78 @@
 # Using Ruby version: ruby 2.7.1p83 (2020-03-31 revision a0c7c23c9c) [x86_64-linux]
 # Your Ruby code here
-class SalesTax
+class Product
+  attr_accessor :name, :imported, :tax_exempted, :price, :price_after_taxes
+  TAX_RATE = 10
+  DUTY_RATE = 5
+  def initialize(name, imported, tax_exempted, price)
+    @name = name
+    @imported = imported
+    @tax_exempted = tax_exempted
+    @price = price.to_i
+    @price_after_taxes = product_total_price
+  end
+  
+  def product_total_price 
+    @price + sales_tax + custom_duty
+  end
+  
+  def is_tax_exempted
+    return @tax_exempted.eql? 'yes'
+  end
+  
+  def is_imported
+    return @imported.eql? 'yes'
+  end
+  
+  def sales_tax
+    is_tax_exempted ? 0 : (@price * TAX_RATE / 100.0)
+  end
+  
+  def custom_duty
+    is_imported ? (@price * DUTY_RATE / 100.0) : 0
+  end
+end
+
+#generating invoice 
+class BillGenerater
+  Questions = ['Name of the product: ', 'Imported?: ', 'Exempted from sales tax?: ', 'Price: ']
   def initialize
-    @items_data = []
+    @product_list = []
     @total_amount = 0
   end
 
-  def take_input(questions)
+  def take_input
     new_item_detail = []
-    questions.each do |question|
+    Questions.each do |question|
       print question
       new_item_detail << gets.chomp
     end
-    new_item_detail
+    Product.new(*new_item_detail)
   end
 
   def add_items
     choice = 'y'
-    while choice == 'y'
-      questions = ['Name of the product: ', 'Imported?: ', 'Exempted from sales tax?: ', 'Price: ']
-      new_detail = take_input(questions)
-      @items_data << new_detail
+    while choice.eql? 'y'
+      new_product = take_input
+      @product_list << new_product
       print 'Do you want to add more items to your list(y/n): '
       choice = gets.chomp
     end
-    show_items
+    show_invoice
   end
 
-  def calculate_total
-    @items_data.each do |item|
-      price = item[3].to_i
-      sale_tax = item[2] == 'no' ? price * 0.1 : 0
-      custom_duty = item[1] == 'yes' ? price * 0.05 : 0
-      @total_amount += (price + sale_tax + custom_duty)
+  def grand_total
+    @product_list.each do |item|
+      @total_amount += item.price_after_taxes
     end
+    @total_amount
   end
 
-  def show_items
-    calculate_total
-    puts "Product name \t   Imported \t Tax_exempted \t   Price"
-    @items_data.each { |item| puts "#{item[0]} \t\t #{item[1]} \t\t #{item[2]} \t\t #{item[3]}" }
-    puts "Total Amount = #{@total_amount.to_i}"
+  def show_invoice
+    puts "Product name \t Imported \t Tax_exempted \t  Price \t Price_after_taxes"
+    @product_list.each { |item| puts "#{item.name} \t\t #{item.imported} \t\t #{item.tax_exempted} \t\t #{item.price} \t\t#{item.price_after_taxes} " }
+    puts "Total Amount = #{grand_total.round(0).to_i}"
   end
 end
 
-SalesTax.new.add_items
+BillGenerater.new.add_items
