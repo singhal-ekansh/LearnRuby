8ba6074ff2394ed1a71a7f9953cd29ff61f94a06
ekansh <ekansin05@gmail.com>
2022-07-08 12:48:39 +0530

    errors fixed

--- exercise17_sum_time/sum_time.rb
+++ exercise17_sum_time/sum_time.rb
@@ -1,31 +1,37 @@
 # Using Ruby version: ruby 2.7.1p83 (2020-03-31 revision a0c7c23c9c) [x86_64-linux]
 # Your Ruby code here
 class Time
-  @@day_count = 0
-  VALID_TIME_REGEXP =  /^([01]?[0-9]|2[0-3]):[0-5]?[0-9]:[0-5]?[0-9]$/.freeze
-  def str_to_seconds(time_in_string)
-    time_array = time_in_string.split(':')
-    hr = time_array[0].to_i
-    min = time_array[1].to_i
-    sec = time_array[2].to_i
-    hr * 60 * 60 + min * 60 + sec
+  VALID_TIME_REGEXP = /^([01]?[0-9]|2[0-3]):[0-5]?[0-9]:[0-5]?[0-9]$/.freeze
+  
+  def self.time_to_seconds(time_object)
+    time_object.hour * 60 * 60 + time_object.min * 60 + time_object.sec
+  end
+  
+  def self.time_array_to_seconds(time_string_array)
+    total_seconds = 0
+    time_string_array.each do |time_string|
+      time_indexs = time_string.split(':')
+      time_obj = Time.new(2000,01,01,time_indexs[0].to_i,time_indexs[1].to_i,time_indexs[2].to_i)
+      total_seconds += time_to_seconds(time_obj)
+    end
+    total_seconds
   end
 
-  def add_time(time_array)
-    return 'Invalid 24-hour time value' if !time_array.all? VALID_TIME_REGEXP
+  def self.add_time(time_array)
+    return 'Invalid 24-hour time value' unless time_array.all? VALID_TIME_REGEXP
 
-    time_now_seconds = str_to_seconds(strftime('%H:%M:%S'))
-    total_seconds = time_array.inject(0) { |memo, time| memo + str_to_seconds(time) }
-    new_time = self + total_seconds - time_now_seconds
-    @@day_count += ((total_seconds)/(24*60*60)).to_i  #calculate days changed
-    new_time.to_s
+    start_time = Time.new(2000,01,01,0,0,0)
+    total_seconds = time_array_to_seconds(time_array)
+    new_time = start_time + total_seconds
+    day_count = total_seconds / (24 * 60 * 60).to_i # calculate days changed
+    display(new_time, day_count)
   end
 
-  def to_s
-    if @@day_count.positive?
-      "#{@@day_count} #{@@day_count>1 ? 'days' : 'day'} & #{strftime('%H:%M:%S')}"
+  def self.display(time, day_count)
+    if day_count.positive?
+      "#{day_count} #{day_count > 1 ? 'days' : 'day'} & #{time.strftime('%H:%M:%S')}"
     else
-      strftime('%H:%M:%S')
+      time.strftime('%H:%M:%S')
     end
   end
 end
@@ -33,5 +39,5 @@ end
 if ARGV.length.zero?
   puts 'Please provide an input'
 else
-  puts Time.new.add_time(ARGV).inspect
+  puts Time.add_time(ARGV).inspect
 end
--- exercise22_name_error/name_error.rb
+++ exercise22_name_error/name_error.rb
@@ -1,21 +1,29 @@
 # Using Ruby version: ruby 2.7.1p83 (2020-03-31 revision a0c7c23c9c) [x86_64-linux]
 # Your Ruby code here
-class NameException < StandardError
+class FirstNameException < StandardError
+end
+class LastNameException < StandardError
+end
+class CapitalizeException < StandardError
 end
 
-class String
-  def validate_name(first_name, last_name)
-    raise NameException, 'Firstname cannot be blank' if first_name.nil? || first_name.length.zero?
-    raise NameException, 'Lastname cannot be blank' if last_name.nil? || last_name.length.zero?
-    raise NameException, 'Firstname must start with uppercase letter' if first_name !~ /[A-Z].*/
+class UserValidation
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  def validate_name
+    raise FirstNameException, 'Firstname cannot be blank' if @first_name.to_s.empty?
+    raise LastNameException, 'Lastname cannot be blank' if @last_name.to_s.empty?
+    raise CapitalizeException, 'Firstname must start with uppercase letter' if @first_name !~ /[A-Z].*/
+
+    "Your name is #{@first_name} #{@last_name}"
     
-    "Your name is #{first_name} #{last_name}"
-  rescue NameException => error
-    error
+  rescue FirstNameException, LastNameException, CapitalizeException => e
+    e
   end
 end
 
 first_name, last_name = ARGV
-puts String.new.validate_name(first_name,last_name)
-    
-    
+puts UserValidation.new(first_name, last_name).validate_name
