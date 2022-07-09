3724e527148806a216bd0b0f0750f7fb8931632c
ekansh <ekansin05@gmail.com>
2022-07-09 16:01:11 +0530

    changes made

--- exercise25_sales_tax/sales_tax.rb
+++ exercise25_sales_tax/sales_tax.rb
@@ -1,41 +1,40 @@
 # Using Ruby version: ruby 2.7.1p83 (2020-03-31 revision a0c7c23c9c) [x86_64-linux]
 # Your Ruby code here
 class Product
-  attr_accessor :name, :imported, :tax_exempted, :price, :price_after_taxes
   TAX_RATE = 10
   DUTY_RATE = 5
+  attr_accessor :name, :imported, :tax_exempted, :price
   def initialize(name, imported, tax_exempted, price)
     @name = name
     @imported = imported
     @tax_exempted = tax_exempted
     @price = price.to_i
-    @price_after_taxes = product_total_price
   end
-  
-  def product_total_price 
+
+  def product_total_price
     @price + sales_tax + custom_duty
   end
-  
-  def is_tax_exempted
-    return @tax_exempted.eql? 'yes'
+
+  def tax_exempted?
+    @tax_exempted == 'yes'
   end
-  
-  def is_imported
-    return @imported.eql? 'yes'
+
+  def imported?
+    @imported == 'yes'
   end
-  
+
   def sales_tax
-    is_tax_exempted ? 0 : (@price * TAX_RATE / 100.0)
+    tax_exempted? ? 0 : (@price * TAX_RATE / 100.0)
   end
-  
+
   def custom_duty
-    is_imported ? (@price * DUTY_RATE / 100.0) : 0
+    imported? ? (@price * DUTY_RATE / 100.0) : 0
   end
 end
 
-#generating invoice 
+# generating invoice
 class BillGenerater
-  Questions = ['Name of the product: ', 'Imported?: ', 'Exempted from sales tax?: ', 'Price: ']
+  QUESTIONS = ['Name of the product: ', 'Imported?: ', 'Exempted from sales tax?: ', 'Price: '].freeze
   def initialize
     @product_list = []
     @total_amount = 0
@@ -43,7 +42,7 @@ class BillGenerater
 
   def take_input
     new_item_detail = []
-    Questions.each do |question|
+    QUESTIONS.each do |question|
       print question
       new_item_detail << gets.chomp
     end
@@ -53,26 +52,23 @@ class BillGenerater
   def add_items
     choice = 'y'
     while choice.eql? 'y'
-      new_product = take_input
-      @product_list << new_product
+      @product_list << take_input
       print 'Do you want to add more items to your list(y/n): '
       choice = gets.chomp
     end
-    show_invoice
   end
 
   def grand_total
-    @product_list.each do |item|
-      @total_amount += item.price_after_taxes
-    end
-    @total_amount
+    @total_amount = @product_list.sum(&:product_total_price)
   end
 
   def show_invoice
     puts "Product name \t Imported \t Tax_exempted \t  Price \t Price_after_taxes"
-    @product_list.each { |item| puts "#{item.name} \t\t #{item.imported} \t\t #{item.tax_exempted} \t\t #{item.price} \t\t#{item.price_after_taxes} " }
+    @product_list.each { |item| puts "#{item.name} \t\t #{item.imported} \t\t #{item.tax_exempted} \t\t #{item.price} \t\t#{item.product_total_price} " }
     puts "Total Amount = #{grand_total.round(0).to_i}"
   end
 end
 
-BillGenerater.new.add_items
+new_invoice = BillGenerater.new
+new_invoice.add_items
+new_invoice.show_invoice
