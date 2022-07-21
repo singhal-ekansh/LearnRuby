3d1eef835c4e4b642e54b788883e5a71f1390211
ekansh <ekansin05@gmail.com>
2022-07-21 11:24:55 +0530

    csv changes

--- exercise23_csv_read/csv_reader_writer.rb
+++ exercise23_csv_read/csv_reader_writer.rb
@@ -1,23 +1,48 @@
 # Using Ruby version: ruby 2.7.1p83 (2020-03-31 revision a0c7c23c9c) [x86_64-linux]
 # Your Ruby code here
 require 'csv'
-class Csvformatter
-  def read_file
-    input_file = CSV.read('input_data.csv', headers: true, header_converters: :symbol)
-    puts input_file
-    output_file = CSV.open('output_data.csv', 'w')
-    map_designation = Hash.new { |h, k| h[k] = [] }
-
-    input_file.each { |row| map_designation[row[:designation]] << [row[:name], row[:empid]] }
-    map_designation.sort.each do |position|
-      output_file << [position[0].strip + (position[1].size > 1 ? 's' : '')]
-      position[1].sort_by { |_name, id| id }.each do |detail|
-        output_file << ["#{detail[0].strip} (Empid: #{detail[1].strip})"] 
-      end
-      output_file << []
-    end
-    output_file.close
+class Employee
+  attr_accessor :name, :empid, :designation
+  def initialize(name, empid, designation)
+    @name = name
+    @empid = empid
+    @designation = designation
   end
 end
 
-Csvformatter.new.read_file
+class EmployeeCsvGenerator
+  
+  def initialize
+    @employee_list = []
+    @input_file = CSV.read('input_data.csv', headers: true, header_converters: :symbol)
+    @output_file = File.open('output_data.txt', 'w')
+  end
+
+  def process
+    read_file
+    generate_csv
+  end
+
+  def read_file
+    @input_file.each { |row| @employee_list.push(Employee.new(row[:name], row[:empid], row[:designation])) }
+  end
+  
+  def add_to_output(designation, employes)
+    @output_file.puts designation.strip + (employes.size > 1 ? 's' : '')
+    employes.each do |person|
+      @output_file.puts "#{person.name.strip} (Empid: #{person.empid.strip})"
+    end 
+    @output_file.puts
+  end
+
+  def generate_csv
+    list = sort_employees
+    list.each {|ele| add_to_output(ele[0], ele[1].sort_by(&:empid))}
+  end
+
+  def sort_employees
+    @employee_list.group_by(&:designation).sort
+  end
+end
+
+EmployeeCsvGenerator.new.process
